09860452a39b067242ed42f309bff206f63318f4
Mark Anthony Tomas <markanthony.tomas.g@bulsu.edu.ph>
2024-06-05 16:03:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <Windows.h>
 using namespace std;
 int main()
-{
+{ 
     choose:
     string seat[]={"x1","y1","x2","y2","x3","y3","x4","y4","x5","y5","x6","y6","x7","y7","x8","y8","x9","y9","x10",
     "y10","x11","y11","x12","y12","x13","y13","x14","y14","x15","y15","x16","y16","x17","y17","x18","y18","x19","y19","x20",
